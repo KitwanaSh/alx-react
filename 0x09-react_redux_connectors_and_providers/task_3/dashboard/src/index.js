d3688a79725ca7238860118b546b6050614201b3
KitwanaSh <rodriguekitweze@gmail.com>
2023-06-24 13:56:07 +0200

    Feat: Update the task_0

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,20 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from "./App/App";
 import uiReducer, { initialState } from "./reducers/uiReducer";
 import { Map } from "immutable";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  uiReducer,
-  Map(initialState),
-  composeEnhancers(applyMiddleware(thunk))
-);
+const store = createStore(uiReducer, Map(initialState), applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
